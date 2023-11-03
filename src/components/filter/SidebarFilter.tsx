09a6726aebee1af40f823a2802af0dc2554705d2
3mmar0 <ammarelgndy6@gmail.com>
2023-11-03 23:59:53 +0200

    nn

--- src/components/filter/SidebarFilter.tsx
+++ src/components/filter/SidebarFilter.tsx
@@ -1,13 +1,29 @@
 import { FC } from "react";
+import InputSelect from "../ui/InputSelect";
+import { CategoryType } from "@/lib/types";
+import { useAppSelector } from "@/store/hooks";
 
 interface SidebarFilterProps {}
 
 const SidebarFilter: FC<SidebarFilterProps> = () => {
+  const cats = useAppSelector((state) => state.GlobalCats);
   return (
-    <div className="md:flex hidden flex-col w-[300px] min-w-[300px] border">
-      <div>
-        <h2>Price</h2>
-        <input type="range" name="price" />
+    <div className="md:flex hidden flex-col w-[300px] min-w-[300px] border p-2">
+      {/* Price */}
+      <div className="pb-3 p-2 border-b border-slate-400">
+        <h2 className="text-xl font-semibold">Price</h2>
+        <input className="w-full" type="range" name="price" />
+      </div>
+      {/* Category */}
+      <div className="pb-5 p-2 border-b border-slate-400">
+        <h2 className="text-xl font-semibold">Category</h2>
+        <InputSelect
+          name="category_id"
+          //   onChange={(e) => setcategory_id(e.target.value)}
+          options={(cats?.data as CategoryType[])?.map((e) => {
+            return { val: e?.id, name: e?.name };
+          })}
+        />
       </div>
     </div>
   );
