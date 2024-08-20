753ce8657fab31e64883711b46dafa2b1d35086a
Ammar_elgendy <139659654+3mmar0@users.noreply.github.com>
2024-08-20 22:13:40 +0300

    Update axios.ts

--- src/slices/axios.ts
+++ src/slices/axios.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://127.0.0.1:8000/api/v1"
-      : "https://estore.ammarelgendy.online/api/v1",
+      : "https://ecommerce-api.ammarelgendy.com/api/v1",
   // withCredentials: true,
 });
 
